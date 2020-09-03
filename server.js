181a0bf50ff237b315a0263fb80b4ffa382ff215
hikutso <62768084+hikutso@users.noreply.github.com>
2020-09-03 14:50:11 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const bodyParser=require("body-parser")
+
 const mongoose  = require('mongoose')
 const PORT = process.env.PORT || 5000
 
+var cors = require('cors')
 
 
 const db=require("./config/keys").mongoURI
@@ -13,10 +16,18 @@ mongoose.connect(db,{useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
     console.log("some error ")
 })
 mongoose.set('useCreateIndex', true)
+app.use(
+    bodyParser.urlencoded({
+      extended: false
+    })
+  )
+app.use(cors())
 
+app.use('/uploads', express.static('uploads'));
 app.use(express.json())
 
 app.use(require("./routes/auth"))
+app.use(require("./routes/post"))
 
 
 
